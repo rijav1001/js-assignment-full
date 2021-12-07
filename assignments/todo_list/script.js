577db4b6494adea50a829b732f54c6e05b2e54ef
rijav1001 <rvu100199@gmail.com>
2021-12-07 18:57:27 +0530

    todo_api

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,17 +1,74 @@
-function toDo() {
-    var temp = document.createElement("p");
-    temp.setAttribute("key", ind);
-    ind += 1;
-    temp.addEventListener("click", function() {
-        this.remove();
-    })
-    var entry = document.getElementsByTagName("input")[0].value;
-    var node = document.createTextNode(entry);
-    var data = document.getElementsByClassName("todos")[0];
-    temp.appendChild(node);
-    data.appendChild(temp);
+function deleteTask() {
+    const temp = document.getElementsByTagName("ul")[0];
+    if(temp) {
+        temp.innerHTML = '';
+    }
+
 }
 
-var ind = 0;
-var added = document.getElementsByTagName("button")[0];
-added.addEventListener('click', toDo);
+async function toDo(){
+
+    const getList = await fetch('https://jsonplaceholder.typicode.com/todos');
+    var data = await getList.json();
+
+    deleteTask()
+    var all = document.getElementById("all").checked
+    var complete = document.getElementById("completed").checked
+    var rendered = document.querySelector(".render-list")
+
+    if(all === true) {
+        for(var i = 0; i < data.length; i++) {
+            const taskAdded = document.createElement("li")
+            var task = document.createTextNode(data[i]["title"])
+            taskAdded.appendChild(task)
+            rendered.appendChild(taskAdded)
+            if(data[i]["completed"] === true) {
+                taskAdded.style["background-color"] = "red";
+            }
+            else {
+                taskAdded.style["background-color"] = "white";
+            }
+
+
+        }
+    }
+    else if(complete === true) {
+
+        for(var i = 0; i < data.length; i++) {
+            if(data[i]["completed"] === true) {
+            const taskAdded = document.createElement("li")
+            var task = document.createTextNode(data[i]["title"])
+            taskAdded.appendChild(task)
+            rendered.appendChild(taskAdded)
+            if(data[i]["completed"] === true) {
+                taskAdded.style["background-color"] = "red";
+            }
+            else {
+                taskAdded.style["background-color"] = "white";
+            }
+        }
+    }
+
+
+    }
+    else {
+        for(var i = 0; i < data.length; i++) {
+            if(data[i]["completed"]==false){
+            const taskAdded = document.createElement("li")
+            var task = document.createTextNode(data[i]["title"])
+            taskAdded.appendChild(task)
+            rendered.appendChild(taskAdded)
+            if(data[i]["completed"] === true) {
+                taskAdded.style["background-color"] = "red";
+            }
+            else {
+                taskAdded.style["background-color"] = "white";
+            }
+        }
+
+
+    }
+}
+}
+
+toDo()
